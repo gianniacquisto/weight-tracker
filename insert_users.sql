8ae3e237ed6101e5e89dd2224e3cec1f6eeef7e3
Gianni Acquisto <gianni.acquisto@gmail.com>
2024-01-21 01:04:57 +0100

    changed from cm to m

--- insert_users.sql
+++ insert_users.sql
@@ -1,3 +1,3 @@
 -- SQLite
-INSERT INTO user VALUES (1, "John", 170.0);
-INSERT INTO user VALUES (2, "Alice", 155.5);
+INSERT INTO user VALUES (1, "John", 1.70);
+INSERT INTO user VALUES (2, "Alice", 1.555);
